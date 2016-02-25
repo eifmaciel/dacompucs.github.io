e79800483e6482b48866a5b210f5a981c03a5bcf
Eliane Maciel <elinefav94@gmail.com>
2016-02-25 11:34:40 -0300

    adicionado animacao

--- js/site.js
+++ js/site.js
@@ -1,3 +1,16 @@
 $(document).on("click", "#menu_open", function(){
     $('#main-navbar').toggleClass('open');
 });
+
+$(document).ready(function() {
+    $('.navbar a').on('click', function(event) {
+        event.preventDefault();
+        var location_anchor = $(this).attr('href');
+        if(location_anchor == '#'){
+            location_anchor = 'html';
+        }
+        $('html, body').animate({
+            scrollTop: $(location_anchor).offset().top
+        }, 1000);
+    });
+});
